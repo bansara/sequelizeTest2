285b0183ac82e3044b9e5753b6ba530e001db40c
Josh Geisler <joshuageisler@gmail.com>
2020-06-29 18:58:59 -0400

    almost there

--- pumpkin.js
+++ pumpkin.js
@@ -2,6 +2,41 @@ const db = require('./dbConfig');
 const Sequelize = require('sequelize');
 
 //YOUR CODE GOES HERE
+const { STRING, BOOLEAN, ENUM } = Sequelize;
+const Pumpkin = db.define('pumpkin', {
+    name: {
+        type: STRING,
+    },
+    size: {
+        type: ENUM,
+        values: ['small', 'medium', 'large']
+    },
+    evil: {
+        type: BOOLEAN,
+        defaultValue: false
+    },
+    carved: {
+        type: BOOLEAN,
+        defaultValue: false
 
+    },
+    candle: {
+        type: BOOLEAN,
+        defaultValue: false
+    }
+})
+
+Pumpkin.addHook('beforeCreate', (pumpkin) => {
+    const coinFlip = Math.random();
+    coinFlip > 0.5 ?
+        pumpkin.name = 'EVIL' + pumpkin.name :
+        pumpkin.name = 'GOOD' + pumpkin.name
+})
+
+Pumpkin.prototype.lightcandle = async function () {
+    await this.update({
+        candle: true
+    });
+}
 //--------------------
 module.exports = Pumpkin;
